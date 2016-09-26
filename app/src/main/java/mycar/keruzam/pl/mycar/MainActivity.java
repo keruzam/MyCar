30050276a65e2dddf2c337eadf3ef15c45a4d6e4
keruzam <mazurek.zg@gmail.com>
2016-09-26 21:32:28 +0200

    * strings spain * onClick ImageButton

--- app/src/main/java/mycar/keruzam/pl/mycar/MainActivity.java
+++ app/src/main/java/mycar/keruzam/pl/mycar/MainActivity.java
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,18 +17,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //BEGIN
+
+        final TextView helloTextView = (TextView) findViewById(R.id.hello);
+        ImageButton pigImageButton = (ImageButton) findViewById(R.id.imageButton);
+
+        pigImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                helloTextView.setText("Co tam ??");
+            }
+        });
+
+        //---------
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
-
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-
         });
     }
 
